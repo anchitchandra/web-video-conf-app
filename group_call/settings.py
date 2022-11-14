32f9f18ff111e63bcd83a62702a4a2c357f9e29e
anchitchandra <anchitchandra111@gmail.com>
2022-11-14 20:57:56 +0530

    revert settings

--- group_call/settings.py
+++ group_call/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # if DEBUG:
 #     env_path = Path('.') / 'group_call/.env'
@@ -94,9 +94,13 @@ if DEBUG:
         "NAME": BASE_DIR / "db.sqlite3",
     }
 else:
-     database =  {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    database = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': env("DATABASE_HOST"),
+        'PORT': env("DATABASE_PORT"),
     }
 
 
